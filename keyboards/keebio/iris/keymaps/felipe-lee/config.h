6475d59216bd711d1dc254a0482cd91d83667e16
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2022-07-09 23:44:05 -0700

    Add first pass at my own custom keymap

---
+++ keyboards/keebio/iris/keymaps/felipe-lee/config.h
@@ -0,0 +1,30 @@
+/*
+Copyright 2022 Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// See https://docs.qmk.fm/#/config_options for all options
+
+// This file will be processed after keyboards/keebio/iris/rev6/config.h
+
+#pragma once
+
+// #define USE_I2C
+//#define EE_HANDS
+
+#define TAPPING_TOGGLE 2 // tap just once for TT() to toggle the layer
+
+// https://docs.qmk.fm/#/feature_caps_word
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
---
+++ keyboards/keebio/iris/keymaps/felipe-lee/rules.mk
@@ -0,0 +1,15 @@
+# https://docs.qmk.fm/#/squeezing_avr
+
+# https://docs.qmk.fm/#/feature_auto_shift
+AUTO_SHIFT_ENABLE = yes
+
+MOUSEKEY_ENABLE = yes        # Mouse keys
+
+# https://docs.qmk.fm/#/feature_caps_word
+CAPS_WORD_ENABLE = yes
+
+# I don't really need these so I'm going to disableThem to save space
+CONSOLE_ENABLE = no
+ENCODER_ENABLE = no
+GRAVE_ESC_ENABLE = no
+SPACE_CADET_ENABLE = no
