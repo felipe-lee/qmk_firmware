fd3f13c7aade4c9346759c87919a37e0fac38818
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2022-07-15 02:32:53 -0700

    Move a few keys around based on issues with usage

--- keyboards/keebio/iris/keymaps/felipe-lee/rules.mk
+++ keyboards/keebio/iris/keymaps/felipe-lee/rules.mk
@@ -1,11 +1,13 @@
 # https://docs.qmk.fm/#/squeezing_avr
 
 # https://docs.qmk.fm/#/feature_auto_shift
+# Can't use at the same time as CAPS_WORD...
 AUTO_SHIFT_ENABLE = yes
 
 MOUSEKEY_ENABLE = yes        # Mouse keys
 
 # https://docs.qmk.fm/#/feature_caps_word
+# Can't use at the same time as AUTO_SHIFT...
 CAPS_WORD_ENABLE = yes
 
 # I don't really need these so I'm going to disableThem to save space
